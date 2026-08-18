b14028d109158cfdb518a12bf63af210982bd5bc
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-18 15:23:11 +0530

    feat(entity/job): add entity for job

--- apps/api/src/job/job.module.ts
+++ apps/api/src/job/job.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Job } from './entities/job.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Job])],
+})
 export class JobModule {}

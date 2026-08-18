86c3546302449ee9d871775fd41c59b395e9a3a8
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-18 10:47:07 +0530

    feat: add new resume module

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { ResumeModule } from './resume/resume.module';
 
 @Module({
-  imports: [],
+  imports: [ResumeModule],
   controllers: [ApiController],
   providers: [ApiService],
 })

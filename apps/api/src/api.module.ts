ceb063a2116c8e9448145d5f324a371f1b89e0f9
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-18 12:58:39 +0530

    feat(type-orm/config): add configration for typeOrm in api.module.ts

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { ResumeModule } from './resume/resume.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ResumeModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ResumeModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USER'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('INIT_DB'),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })

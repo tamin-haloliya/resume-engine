0c2cc66b3c5b16505be66e3ea67b84a17967b468
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-18 12:23:49 +0530

    feat: configure config module for global

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { ResumeModule } from './resume/resume.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ResumeModule],
+  imports: [ResumeModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [ApiController],
   providers: [ApiService],
 })

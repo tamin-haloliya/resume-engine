7aeabb1df7d683c6dd01a4685e2195c5d6cf08af
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-19 09:49:48 +0530

    feat: add global validation pipe and api prefix api/v1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();

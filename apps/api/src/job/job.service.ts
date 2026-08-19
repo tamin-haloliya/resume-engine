56df971fcc2a971f19ef839f9932fa7c1abe65d3
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-19 09:52:09 +0530

    feat(job/service): add service for creating and saving new job into database

---
+++ apps/api/src/job/job.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CreateJobDto } from './dto/create-job.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Job } from './entities/job.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class JobService {
+  constructor(
+    @InjectRepository(Job)
+    private readonly jobRepository: Repository<Job>,
+  ) {}
+
+  async create(createJobDto: CreateJobDto): Promise<Job> {
+    const newJob = this.jobRepository.create(createJobDto);
+    return await this.jobRepository.save(newJob);
+  }
+}

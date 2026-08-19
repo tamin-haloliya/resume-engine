9626668dc4e88d9e397eea91a89ae9c7869cd168
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-19 09:51:31 +0530

    feat(job/module): configure job controller and job service into job module

--- apps/api/src/job/job.module.ts
+++ apps/api/src/job/job.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './entities/job.entity';
+import { JobController } from './job.controller';
+import { JobService } from './job.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Job])],
+  controllers: [JobController],
+  providers: [JobService],
 })
 export class JobModule {}

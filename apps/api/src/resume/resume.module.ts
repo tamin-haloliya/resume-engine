6c02caff7a9119b0e7101eccafbb678b8c442fb2
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-22 11:29:46 +0530

    feat(resume): integrate StorageModule and add ResumeController and ResumeService

--- apps/api/src/resume/resume.module.ts
+++ apps/api/src/resume/resume.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Resume } from './entities/resume.entity';
+import { ResumeController } from './resume.controller';
+import { ResumeService } from './resume.service';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Resume])],
+  imports: [TypeOrmModule.forFeature([Resume]), StorageModule],
+  controllers: [ResumeController],
+  providers: [ResumeService],
 })
 export class ResumeModule {}

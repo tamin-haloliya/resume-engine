3f6bebee14dc6f3c8df567624714e0623e4653ed
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-28 14:58:03 +0530

    feat(extractor): export extractor service

--- apps/api/src/extractor/extractor.module.ts
+++ apps/api/src/extractor/extractor.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ExtractorService } from './extractor.service';
 
 @Module({
-  providers: [ExtractorService]
+  providers: [ExtractorService],
+  exports: [ExtractorService],
 })
 export class ExtractorModule {}

2eab7683dc61b3edb56291feddcf34a5c6edeb0c
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-08-28 14:51:15 +0530

    feat(extractor): add extract skill for extracting skills from raw text

--- apps/api/src/extractor/extractor.service.ts
+++ apps/api/src/extractor/extractor.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { escapeRegex } from './utils/escape-regex';
 
 @Injectable()
-export class ExtractorService {}
+export class ExtractorService {
+  extractSkill(rawText: string, dict: string[]): string[] {
+    return dict.filter((skill) => {
+      const pattern = new RegExp(`\\b${escapeRegex(skill)}\\b`, 'i');
+      return pattern.test(rawText);
+    });
+  }
+}

cf64bd5d47a2e1a77d3c9ca4be56a5bfc6f75741
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-02 17:45:56 +0530

    feat(job-matching): initialize job and resume entities

--- apps/api/src/matching/matching.module.ts
+++ apps/api/src/matching/matching.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MatchingService } from './matching.service';
+import { Resume } from '../resume/entities/resume.entity';
+import { Job } from '../job/entities/job.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [MatchingService]
+  imports: [TypeOrmModule.forFeature([Resume, Job])],
+  providers: [MatchingService],
 })
 export class MatchingModule {}

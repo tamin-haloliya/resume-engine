6dc8341860d2d5c20fa7a92602b1f749fbc8645d
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-02 18:42:55 +0530

    feat(job-matching): add controller in module

--- apps/api/src/matching/matching.module.ts
+++ apps/api/src/matching/matching.module.ts
@@ -3,9 +3,11 @@ import { MatchingService } from './matching.service';
 import { Resume } from '../resume/entities/resume.entity';
 import { Job } from '../job/entities/job.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MatchingController } from './matching.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Resume, Job])],
   providers: [MatchingService],
+  controllers: [MatchingController],
 })
 export class MatchingModule {}

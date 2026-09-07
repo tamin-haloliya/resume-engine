dbb3c8009dbc21fcc3c2732a96637cc0d885b86a
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-07 21:04:12 +0530

    refactor(shared-env): remove env-validation module

--- libs/env-validation/src/env-validation.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EnvValidationService } from './env-validation.service';
-
-@Module({
-  providers: [EnvValidationService],
-  exports: [EnvValidationService],
-})
-export class EnvValidationModule {}

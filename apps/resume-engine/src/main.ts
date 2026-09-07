2620b1c7acbf382151d5aece7fa8f3d620010b83
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-07 21:28:22 +0530

    fix(shared-env): correct environment variable name for application port

--- apps/resume-engine/src/main.ts
+++ apps/resume-engine/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.RESUME_ENGINE_PORT ?? 3000);
 }
 bootstrap();

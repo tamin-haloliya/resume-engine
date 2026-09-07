bb2ddb83e0d8b879cc289e34237b4be6fb45217b
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-07 14:23:44 +0530

    feat(notification-app): intialize rabbitmq in notification module

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
-  imports: [],
+  imports: [
+    RabbitMQModule.forRoot({
+      exchanges: [{ name: 'resume.events', type: 'topic' }],
+      uri: 'amqp://15672',
+    }),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
 })

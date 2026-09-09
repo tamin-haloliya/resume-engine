aa13a5311aa1f8400db55b1eb3fe072d86ad3a6c
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-09 15:33:59 +0530

    feat(minio-storage): implement bucket initialization in minio service

--- apps/resume-engine/src/storage/minio-storage/minio-storage.service.ts
+++ apps/resume-engine/src/storage/minio-storage/minio-storage.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { MINIO_CLIENT } from './minio.provider';
+import { Client } from 'minio';
 
 @Injectable()
-export class MinioStorageService {}
+export class MinioStorageService implements OnModuleInit {
+  constructor(@Inject(MINIO_CLIENT) private readonly client: Client) {}
+
+  BUCKET_NAME = 'resumes';
+
+  async onModuleInit() {
+    try {
+      const exists = await this.client.bucketExists(this.BUCKET_NAME);
+      if (!exists) {
+        await this.client.makeBucket('my-bucket', 'us-east-1');
+      }
+      console.log('minio connection established!');
+    } catch {
+      console.log('minio connection failed');
+    }
+  }
+}

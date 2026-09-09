4662a0738a0001aaa5d4a23d7b08b027bfa92ddc
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-09 15:07:36 +0530

    feat(storage): add minio storage service and provider config

--- apps/resume-engine/src/storage/storage.module.ts
+++ apps/resume-engine/src/storage/storage.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DiskStorageService } from './disk-storage/disk-storage.service';
 import { STORAGE_SERVICE } from './storage.interface';
+import { ConfigModule } from '@nestjs/config';
+import { MinioStorageService } from './minio-storage/minio-storage.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [
     {
       provide: STORAGE_SERVICE,
       useClass: DiskStorageService,
     },
+    MinioStorageService,
   ],
   exports: [STORAGE_SERVICE],
 })
